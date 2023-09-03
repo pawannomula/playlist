0a43b017365aadf0885b80d7f6ec8c7107b3c620
pawannomula <pawan.nomula@outlook.com>
2023-09-03 08:51:47 +0530

    Update PlayList.java

--- PlayList.java
+++ PlayList.java
@@ -1,1 +1,30 @@
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PlayList {
+    // static class
+    public static class sizeLimitedQue extends LinkedList {
+        private int sizeofQ;
+        public sizeLimitedQue(int sizeofQ) {
+            this.sizeofQ = sizeofQ;
+        }
+
+        @Override
+        public boolean add(Object o) {
+            while (this.size() == sizeofQ) {
+                super.remove();
+            }
+            return super.add(o);
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue songsQ = new sizeLimitedQue(3);
+        String[] songs = {"song1", "song2", "song3","song4","song5"};
+        for (String song : songs) {
+            songsQ.add(song);
+        }
+        System.out.println(songsQ);
+    }
+}
